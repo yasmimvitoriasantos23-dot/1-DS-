7ecac61238cf098f9d3b3a016a91fd2c4246515c
yasmimvitoriasantos23-dot <yasmim.vitoria.santos23@escola.pr.gov.br>
2026-06-18 08:35:03 -0300

    mudei o contraste para amarelo

--- script.js
+++ script.js
@@ -11,8 +11,17 @@ const botão = document.querySelector("button")
 const tamanho = document.getElementById("tamanho")
 tamanho.addEventListener("click", aumentarFonte)
 let fonte =16;
+
 //FUNÇÃO
 function aumentarFonte(){
     fonte++;
     document.body.style.fontSize = fonte + "px";
+}
+// BOTÃO DE CONTRASTE
+const contraste = document.getElementById("contraste")
+contraste.addEventListener("click",mudar_o_contraste_para_amarelo )
+
+//FUNÇAO
+function mudar_o_contraste_para_amarelo(){
+    document.body.style.color = "yellow"
 }
